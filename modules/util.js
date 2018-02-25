bb1d1ccdf29f1e44d5248d6a638e10d4b92a6844
Lance <lancelsims@gmail.com>
2018-02-25 09:46:37 -0600

    [CHK-0001] fixed eslint issue and uppercased the returned string

--- modules/util.js
+++ modules/util.js
@@ -3,9 +3,10 @@ module.exports = (() => {
         return new Buffer(originalString).toString('base64');
     };
 
+    // product owner said we have to uppercase all reversed strings
     const reverseMe = (originalString) => {
-        return originalString.split('').reverse().join().replace(/,/g, '');
-    }
+        return originalString.split('').reverse().join().replace(/,/g, '').toUpperCase();
+    };
 
     return {
         base64Encode,
